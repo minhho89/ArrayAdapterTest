dc698bf9e7c80df5bef1ea868bbba68001e733e7
minhho89 <61931543+minhho89@users.noreply.github.com>
2021-02-25 17:23:27 +0700

    add last editText event
    add onclick event to last editText

--- app/src/main/java/minhfx03283/funix/arrayadaptertest/QuizAdapter.java
+++ app/src/main/java/minhfx03283/funix/arrayadaptertest/QuizAdapter.java
@@ -21,7 +21,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
