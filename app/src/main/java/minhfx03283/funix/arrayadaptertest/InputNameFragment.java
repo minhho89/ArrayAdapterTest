86e892469111671a27a95c899ff47abaa706b83f
minhho89 <61931543+minhho89@users.noreply.github.com>
2021-02-26 15:10:21 +0700

    Merge branch 'main' of https://github.com/minhho89/ArrayAdapterTest into main
    # Conflicts:
    #	app/src/main/java/minhfx03283/funix/arrayadaptertest/Quiz/QuizType2.java
    #	app/src/main/java/minhfx03283/funix/arrayadaptertest/QuizAdapter.java
    
    fixed checkbox

---
+++ app/src/main/java/minhfx03283/funix/arrayadaptertest/InputNameFragment.java
@@ -0,0 +1,95 @@
+package minhfx03283.funix.arrayadaptertest;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class InputNameFragment extends DialogFragment {
+    // Use this instance of the interface to deliver action events
+    NoticeDialogListener listener;
+    private String userName;
+
+    public InputNameFragment() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // Use Builder class to build dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // Get Layout Inflater & EditText reference
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.dialog_name_input, null);
+        EditText editText = (EditText) dialogLayout.findViewById(R.id.edt_name_input);
+
+        // Get activity_main inflater
+        View mainLayout = inflater.inflate(R.layout.activity_main, null);
+
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(dialogLayout)
+                // Add action buttons
+                .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        InputNameFragment.this.setUserName(editText.getText().toString());
+                        listener.onDialogPositiveClick(InputNameFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        InputNameFragment.this.setUserName("User");
+                        listener.onDialogNegativeClick(InputNameFragment.this);
+                        InputNameFragment.this.getDialog().cancel();
+                    }
+                });
+
+
+        return builder.create();
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (NoticeDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(this.getActivity().toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface NoticeDialogListener {
+        void onDialogPositiveClick(InputNameFragment dialog);
+
+        void onDialogNegativeClick(InputNameFragment dialog);
+    }
+}
